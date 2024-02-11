842c47a9263076348905656eacf2b8aa5f2287a2
VernPherus <vernpherus@gmail.com>
2024-02-11 16:52:25 +0800

    fixed tailwind dependencies

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import './style.css'
-import './index.css'
+import './input.css'
 
 
 createApp(App)
--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
+        './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
     ],
+    darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},
     },
-    plugins: [],
+    variants: {
+      extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
